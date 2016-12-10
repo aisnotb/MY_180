18cfcb601813b7b084a967e957a0e88b14e298fc
huageng <stehliu25@gmail.com>
2016-12-10 15:36:17 -0500

    changed some questions

--- DAY1/script.js
+++ DAY1/script.js
@@ -1,1 +1,22 @@
-var scores = document.
+$(document).ready(function(){
+	var message = "";
+
+	var score = getScore();
+
+	function getScore(){
+		var scoreSum = 0;
+		var numOfBoxes = $('input[type=radio]');
+		console.log(numOfBoxes);
+		for (var i = 0; i < numOfBoxes.length; i++) {
+			scoreSum+=numOfBoxes[i];
+			console.log(scoreSum);
+		}
+		return scoreSum;
+	}
+
+	$("#submit").on('click', function(){
+		alert("button clicked"); 
+		alert(getScore());
+	});
+
+});

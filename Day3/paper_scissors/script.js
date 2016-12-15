ac46c05f75efa7432c2e76fe5f660d4c4304455b
huageng <stehliu25@gmail.com>
2016-12-15 14:40:54 -0500

    add vendor prefixes

--- Day3/paper_scissors/script.js
+++ Day3/paper_scissors/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 	$(".button").on("click", function(){
-		alert("hellow");
+		// alert("hellow");
+
+		var choices = ["paper", "hammer", "scissors"];
+
+		var computerChoice = choices[(Math.random()*3)];
+
+
+		//add evenet Listener to the butttons
+
 	});
+
+	$("#rock").on("click", function(){
+		
+	})
 });

7351e616b151e2c35808191e654c802dc37a0a2f
huageng <stehliu25@gmail.com>
2016-12-16 20:11:46 -0500

    fixed some bugs

--- Day3/paper_scissors/script.js
+++ Day3/paper_scissors/script.js
@@ -1,53 +1,72 @@
 $(document).ready(function(){
-	// $(".button").on("click", function(){
-	// 	// alert("hellow");
 
-	// 	var choices =
+	var userChoice, computerChoice;
+	var choices =  ["paper", "rock", "scissors"];
 
-	// 	var computerChoice = choices[Math.floor((Math.random()*3))];
-
-
-	// 	//add evenet Listener to the butttons
-
-	// });
-
-	var userChoice ;
-
-	$('button').on("click", go);
+	$('.button').on("click", go);
 
 	function go(){
+
 		console.log(this);
 		userChoice = this.id;
-	}
-
-	var choices =  ["paper", "hammer", "scissors"];
-
-	var computerChoice = choices[Math.floor(Math.random()*3)];
-
-
-	function displayResult(){
-		if (computerChoice == "paper" && userChoice == "siccsor") {
-			alert("you lost");
-		}else if(computerChoice=='')
-
-	}
-
-	$("#rock").on("click", function(){
+		$(".button").off("click");  //
+		console.log("user choice is "+ userChoice);
+		// $("#result").hide();	
+		makeChoice();
 		countDown();
-		function countDown(){
-			var i= 3;
-			count();
-			function count(){
-				$('#showResult').text(i);
-				if(i==0){
-					displayResult();
-				}else{
-					i-=1;
-					//wait for 500ms
-					setTimeout(count,500);
-					console.log("executed once");
-				}
+		displayResult();
+	}
+
+	function makeChoice(){
+		computerChoice = choices[Math.floor(Math.random()*3)];
+		console.log("computer choice is "+ computerChoice);
+	}
+
+	function countDown(){
+		var i= 3;
+		$("#left-fist-png").addClass("animateImages");
+		$("#right-fist-png").addClass("animateImages");
+		count();
+		function count(){
+			if(i==3){
+				$("showResult").text("shoot")
+			}
+			$('#showResult').text(i);
+			if(i==0){
+				displayResult();
+			}else{
+				i-=1;
+				//wait for 500ms
+				setTimeout(count,1000);
 			}
 		}
-	});
+	}
+
+	function displayResult(){
+		$("#left-fist-png").removeClass("animateImages");
+		$("#left-container").hide("slow", function(){
+			console.log("left container hided");
+		});
+
+		$("#right-container").hide("slow", function(){
+			console.log("right container hided");
+		});
+		$("#left-"+computerChoice+"-png").show("fast", function(){
+			console.log("computer choice showed");
+		});
+		$("#right-"+ userChoice+"-png").show();
+
+		setTimeout(function(){
+			if(userChoice== computerChoice){
+				$("#showResult").text("Tie");
+			}else{
+				if((userChoice=="rock" && computerChoice=='scissors') || (userChoice=='paper' && computerChoice=='rock') ||(userChoice=='scissors' && computerChoice=='paper')){
+					$("#showResult").text("you win");
+				}else{
+					$("#showResult").text("you lose");
+				}
+			}
+			$('.button').on('click', go);
+		},5000);
+	}
 });

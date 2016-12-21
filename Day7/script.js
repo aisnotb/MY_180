6034f9f127f6daadee91a49ec8002b10f7f55f1f
huageng <stehliu25@gmail.com>
2016-12-21 15:49:43 -0500

    show pips

---
+++ Day7/script.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+	$("button").on('click', function(){
+		showDice();
+	});
+
+	function reset(){
+		$(".dot").hide();
+	}
+
+	function generateRandom(){
+		//get a number between 1 - 19
+		var rdm= [];
+		var rdm1 = Math.floor(Math.random()*6+1);
+		var rdm2 = Math.floor(Math.random()*6+1);
+		rdm.push(rdm1);
+		rdm.push(rdm2);
+		return rdm;
+	}
+
+	function showDice(){
+		reset();
+		var selecte = generateRandom();
+		var dice1 = '#dot-' + selecte[0];
+		var dice2 = "#dot-" + selecte[1];
+		console.log(selecte[0]);
+		console.log(selecte[1]);									
+		if (selecte[0] == 1) {
+			$("#dot-5").show();
+		}
+		if(selecte[1] == 1){
+			$("#dot-15").show();
+		}
+
+
+		if (selecte[0]==2) {
+			$("#dot-2").show();
+			$("#dot-8").show();
+		}
+		if (selecte[1]==2) {
+			$("#dot-12").show();
+			$("#dot-18").show();
+		}
+
+
+		if (selecte[0]==3) {
+			for (var i = 3; i <=7; i=i+2) {
+				var dots = "#dot-" + i;
+				$(dots).show();
+			}
+		}
+
+		if (selecte[1]==3) {
+			for (var i = 13; i<=17; i=i+2) {
+				var dots = "#dot-" + i;
+				$(dots).show();
+			}
+		}
+
+		if(selecte[0] == 4){
+			for(var i = 1; i <=9&&i!=5; i+=2){
+				var dots = "#dot-" + i;
+				$(dots).show();
+			}
+		}
+		if(selecte[1] == 4){
+			for(var i = 11; i <=19&&i!=15; i+=2){
+				var dots = "#dot-" + i;
+				$(dots).show();
+			}
+		}
+
+		if(selecte[0] == 5){
+			for(var i = 1; i <=9; i+=2){
+				var dots = "#dot-" + i;
+				$(dots).show();
+			}
+		}
+		if(selecte[1] == 5){
+			for(var i = 11; i <=19; i+=2){
+				var dots = "#dot-" + i;
+				$(dots).show();
+			}
+		}
+
+		if(selecte[0] == 6){
+			for (var i = 1; i <= 6; i++) {
+				var dots = "#dot-"+i;
+				$(dots).show();
+			}
+		}
+
+		if(selecte[1] == 6){
+			for (var i = 11; i <= 16; i++) {
+				var dots = "#dot-"+i;
+				$(dots).show();
+			}
+		}
+
+	}
+});

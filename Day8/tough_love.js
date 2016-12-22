189db1402a4233559f558dded7dff8d8ad1e8b7d
huageng <stehliu25@gmail.com>
2016-12-22 17:58:03 -0500

    small tweaks

--- Day8/tough_love.js
+++ Day8/tough_love.js
@@ -1,7 +1,30 @@
-$(document).ready(function(){
-	$("#inputbox").on("submit", function(event){
-		alert("yoyoyo");
-		console.log("dfsdfdsfsd");
-		event.preventDefault();
+$(document).ready(function() {
+    var advices = [
+	    "That's terrible! You should knock that off!",
+	    "Seriously? Why don't you grow the hell up?!",
+	    "Aren't you a little old for that crap?",
+	    "You are bad and you should feel bad!",
+	    "Gross. You are gross.",
+	    "Are you going to suck all your life?",
+	    "What? Who does that?!",
+	    "I thought you were better than that.",
+	    "My disapproval is overwhelming.",
+	    " ಠ__ಠ 你的脑袋被门夹了？ ",
+	    "Are you freaking kidding me?",
+	    "NO! Bad!",
+	    "And when do you plan on becoming an adult?",
+	    "That is totally unacceptable.",
+	    "You should be utterly ashamed.",
+	    "Ugh! That's horrible!",
+	    "A kitten dies everytime you do that.",
+	    "I can't believe you are that disgusting."
+	];
+
+	$("#inputbox").on("submit", function(event) {
+	    console.log("dfsdfdsfsd");
+	    event.preventDefault();
 	});
-})
+
+	
+
+});
